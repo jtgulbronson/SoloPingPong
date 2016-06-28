be2bb22614f039d619821984b80e8e49a58d02d2
Jordan Gulbronson <jordangulbronson11@gmail.com>
2016-06-28 14:48:35 -0400

    Week02 changes
    Starting to build solo pong game. canvas fill and mouse tracking

--- js/game.js
+++ js/game.js
@@ -1,22 +1,51 @@
 // RequestAnimFrame: a browser API for getting smooth animations
-window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame   ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame    ||
-			window.oRequestAnimationFrame      ||
-			window.msRequestAnimationFrame     ||
-		function( callback ){
-			return window.setTimeout(callback, 1000 / 60);
-		};
+window.requestAnimFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    };
 })();
 
-window.cancelRequestAnimFrame = ( function() {
-	return  window.cancelAnimationFrame          ||
-			window.webkitCancelRequestAnimationFrame ||
-			window.mozCancelRequestAnimationFrame    ||
-			window.oCancelRequestAnimationFrame      ||
-			window.msCancelRequestAnimationFrame     ||
-		clearTimeout;
-} )();
+window.cancelRequestAnimFrame = (function () {
+  return window.cancelAnimationFrame ||
+    window.webkitCancelRequestAnimationFrame ||
+    window.mozCancelRequestAnimationFrame ||
+    window.oCancelRequestAnimationFrame ||
+    window.msCancelRequestAnimationFrame ||
+    clearTimeout;
+})();
 
-console.log('Holla');
+//Do Not Touch Above Code
+
+//console.log('Holla');
+
+// Step one .. jtg .. Create game canvas and track mouse position
+var gameCanvas = document.getElementById("canvas");
+//Store HTML5 canvas tag into JS variable
+var ctx = gameCanvas.getContext("2d"); //Create context 2d
+var W = window.innerWidth;
+var H = window.innerHeight;
+
+var mouseObj = {};
+
+gameCanvas.width = W;
+gameCanvas.height = H;
+
+function paintCanvas() {
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, W, H);
+}
+paintCanvas();
+
+//Track Mouse Position
+function trackPosition(evt) {
+  mouseObj.x = evt.pageX;
+  mouseObj.y = evt.pageY;
+  console.log("Cursor x is : " + mouseObj.x + " Cursor y is : " + mouseObj.y)
+}
+
+gameCanvas.addEventListener("mousemove", trackPosition, true);
